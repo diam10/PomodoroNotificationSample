574c98267534bfb709e73e360783f2b272cb65b4
diam10 <diam.cccc@gmail.com>
2025-09-23 00:22:36 +0900

    Initial Commit

---
+++ PomodoroNotificationSample/PomodoroNotificationSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroNotificationSampleApp.swift
+//  PomodoroNotificationSample
+//
+//  Created by diam on 2025/09/23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroNotificationSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
